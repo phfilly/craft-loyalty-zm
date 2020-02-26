6377ce08b83e15fdfd8010127dd284f5c6a30722
Philip <philipdt@bbd.co.za>
2020-02-26 08:42:13 +0200

    Add new offers

--- craft-zm/src/app/content/clientData.ts
+++ craft-zm/src/app/content/clientData.ts
@@ -7,12 +7,20 @@ export const DATA = [
       {
         link: 'cruises.jpg',
         name: 'Cruises',
-        text: '',
+        text:
+          'Pay with your Mastercard and get 10% discount on cabin cost only for any 7 night and longer sailings onboard Royal Caribbean International, Celebrity Cruises & Azamara bookings',
         pdf: 'https://www.cruises.co.za/'
       },
       {
         link: 'Flight_Centre_Canada_Logo-wikipedia.png',
         name: 'Flight Centre',
+        text:
+          'Pay with your Mastercard and get 5% off any package or flight for Zimbabwe',
+        pdf: 'https://www.flightcentre.co.za/'
+      },
+      {
+        link: 'RTG.png',
+        name: 'Rainbow Tourism Group',
         text: '',
         pdf: 'https://www.flightcentre.co.za/'
       }
