f76adc1e8cd523180da89d388be0950e2ef06e98
phfilly <philipdutoit@yahoo.com>
2020-04-10 09:54:00 +0200

    Update images

--- craft-zm/src/app/content/clientData.ts
+++ craft-zm/src/app/content/clientData.ts
@@ -2,121 +2,121 @@ export const DATA = [
   {
     key: 'travel',
     slogan: '',
-    background: 'images/travel-2.jpg',
+    background: 'images/travel-main.jpg',
     clients: [
       {
         link: 'cruises.jpg',
         name: 'Cruises',
         text:
           'Pay with your Mastercard and get 10% discount on cabin cost only for any 7 night and longer sailings onboard Royal Caribbean International, Celebrity Cruises & Azamara bookings',
-        url: 'https://www.cruises.co.za/'
+        url: 'https://www.cruises.co.za/',
       },
       {
         link: 'Flight_Centre.jpg',
         name: 'Flight Centre',
         text: 'Pay with your Mastercard and get 5% off any package or flight',
-        url: 'https://www.flightcentre.co.za/'
-      }
-    ]
+        url: 'https://www.flightcentre.co.za/',
+      },
+    ],
   },
   {
     key: 'accommodation',
     slogan: '',
-    background: 'images/travel-2.jpg',
+    background: 'images/accommodation-main.jpg',
     clients: [
       {
         link: 'offer1.PNG',
         name: 'A Zambezi River Lodge',
         text:
           'A’Zambezi River Lodge is a 6-time (consecutive) TripAdvisor service excellence award, winner',
-        textAvatar: '../../assets/Logos/RTG_mini.PNG'
+        textAvatar: '../../assets/logos/RTG_mini.PNG',
       },
       {
         link: 'offer2.PNG',
         name: 'Victoria Falls Rainbow Hotel',
         text:
           'The hotel’s central location means that you’re within walking distance of the mighty Victoria Falls and other awesome activities around the resort town',
-        textAvatar: '../../assets/Logos/RTG_mini.PNG'
+        textAvatar: '../../assets/logos/RTG_mini.PNG',
       },
       {
         link: 'offer3-new.PNG',
         name: 'Kadoma Hotel and Conference Centre',
         text:
           'Kadoma Hotel & Conference Centre is the ideal getaway destination for both conferencing and leisure',
-        textAvatar: '../../assets/Logos/RTG_mini.PNG'
+        textAvatar: '../../assets/logos/RTG_mini.PNG',
       },
       {
         link: 'offer4.PNG',
         name: 'Rainbow Towers Hotel & Conference Centre',
         text:
           'The luxurious iconic 5-star hotel offers a unique and refreshing experience for both business and leisure customers in the sunshine city',
-        textAvatar: '../../assets/Logos/RTG_mini.PNG'
+        textAvatar: '../../assets/logos/RTG_mini.PNG',
       },
       {
         link: 'offer5.PNG',
         name: 'New Ambassador Hotel',
         text:
           'The New Ambassador Hotel has a rich history as the epicenter of black consciousness. Located in Harare’s Central Business District, the boutique hotel is within reach of restaurants, cinemas, the National Art Gallery and other service centers',
-        textAvatar: '../../assets/Logos/RTG_mini.PNG'
+        textAvatar: '../../assets/logos/RTG_mini.PNG',
       },
       {
         link: 'offer6-new.PNG',
         name: 'Monomotapa Hotel Harare',
         text:
           'The facilities at Monomotapa are specially designed for the convenience of the guest. The hotel provides a one-stop service where accommodation and other services are all available',
-        textAvatar: '../../assets/Logos/RTG_mini.PNG'
+        textAvatar: '../../assets/logos/RTG_mini.PNG',
       },
       {
         link: 'offer7.PNG',
         name: 'Bulawayo Rainbow Hotel',
         text:
           'The hotel is a 15 minute drive from Joshua Mqabuko Nkomo Airport and is within reach of express coaches connecting Harare and Victoria Falls',
-        textAvatar: '../../assets/Logos/RTG_mini.PNG'
+        textAvatar: '../../assets/logos/RTG_mini.PNG',
       },
       {
         link: 'offer8-new.PNG',
         name: 'Cresta Lodge Harare',
         text:
           'Set in beautiful and tranquil surroundings, located on the outskirts of the dynamic city centre, you’ll find the beautiful Cresta Lodge, one of the best hotels in Harare',
-        textAvatar: '../../assets/Logos/RTG_mini.PNG'
+        textAvatar: '../../assets/logos/RTG_mini.PNG',
       },
       {
         link: 'offer9-new.PNG',
         name: 'Cresta Spray View Victoria Falls',
         text:
           'Topping the list of affordable Victoria Falls hotels, Cresta Sprayview offers stylish, well-appointed accommodation and a host of convenient facilities for business and leisure travelers alike',
-        textAvatar: '../../assets/Logos/RTG_mini.PNG'
-      }
-    ]
+        textAvatar: '../../assets/logos/RTG_mini.PNG',
+      },
+    ],
   },
   {
     key: 'dining',
     slogan: '',
-    background: 'images/travel-2.jpg',
+    background: 'images/dining-main.jpg',
     clients: [
       {
         link: 'offer10.jpg',
         name: 'Le Rendezvous',
         text:
-          'Situated on the Mezzanine Floor, the coffee shop offers a wide range of coffee and teas along with a selection of light meals, cakes and pastries. The coffee shop is ideal for relaxing with family and friends or for a business meeting'
+          'Situated on the Mezzanine Floor, the coffee shop offers a wide range of coffee and teas along with a selection of light meals, cakes and pastries. The coffee shop is ideal for relaxing with family and friends or for a business meeting',
       },
       {
         link: 'restaurant.jpeg',
         name: 'Mojos',
-        text: "Come to Mojo's and eat as much meat as you want"
+        text: "Come to Mojo's and eat as much meat as you want",
       },
       {
         link: 'Kombahari.PNG',
         name: 'Kombahari Restaurant',
         text:
-          'The best Afro-Asian fusion dining experience and only live cooking restaurant in the country.'
+          'The best Afro-Asian fusion dining experience and only live cooking restaurant in the country.',
       },
       {
         link: 'restaurant2.jpeg',
         name: 'Alo Alo',
         text:
-          'A Home away from Home Experience surrounded by a beautiful tranquil garden'
-      }
-    ]
-  }
+          'A Home away from Home Experience surrounded by a beautiful tranquil garden',
+      },
+    ],
+  },
 ];
