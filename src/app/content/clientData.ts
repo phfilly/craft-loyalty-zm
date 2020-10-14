36dbfb9980ac631c7b9cae0c10780f3cccc18e8e
phfilly <philipdutoit@yahoo.com>
2020-10-14 13:56:29 +0200

    Update images and styling

--- src/app/content/clientData.ts
+++ src/app/content/clientData.ts
@@ -5,20 +5,20 @@ export const DATA = [
     background: 'images/travel-main.jpg',
     clients: [
       {
-        link: 'cruises.jpg',
+        link: 'logos/cruises.jpg',
         name: 'Cruises',
         text:
           'Pay with your Mastercard and get 10% discount on cabin cost only for any 7 night and longer sailings onboard Royal Caribbean International, Celebrity Cruises & Azamara bookings',
         url: 'https://www.cruises.co.za/',
       },
       {
-        link: 'Flight_Centre.jpg',
+        link: 'logos/Flight_Centre.jpg',
         name: 'Flight Centre',
         text: 'Pay with your Mastercard and get 5% off any package or flight',
         url: 'https://www.flightcentre.co.za/',
       },
       {
-        link: 'health-spa-logo.jpg',
+        link: 'logos/health-spa-logo.jpg',
         name: 'Health Spas',
         text: 'Pay with your MasterCard and receive 10% off your Health spa voucher (redeemable at any of the 2 spas affiliated to Health Spa) ',
         url: null,
@@ -32,7 +32,7 @@ export const DATA = [
     logo: 'FC-logo.png',
     clients: [
       {
-        link: '../images/flight-centre/Sun City Cabanas.jpg',
+        link: 'images/flight-centre/sc.jpg',
         id: 1,
         name: 'Flight Centre',
         title: 'Sun City Cabanas Weekend Rate',
@@ -43,7 +43,7 @@ export const DATA = [
         specialPrice: 2685
       },
       {
-        link: '../images/flight-centre/Sun City Cabanas pool 2.jpg',
+        link: 'images/flight-centre/sc-pool.jpg',
         id: 2,
         name: 'Flight Centre',
         title: 'Sun City Cabanas mid-week rate',
@@ -54,7 +54,7 @@ export const DATA = [
         specialPrice: 2225
       },
       {
-        link: '../images/flight-centre/Soho Hotel Room.jpg',
+        link: 'images/flight-centre/sh-hotel-room.jpg',
         id: 3,
         name: 'Flight Centre',
         title: 'Sun City Soho Weekend',
@@ -65,7 +65,7 @@ export const DATA = [
         specialPrice: 4055
       },
       {
-        link: '../images/flight-centre/Soho Hotel Pool.jpg',
+        link: 'images/flight-centre/sh-room.jpg',
         id: 4,
         name: 'Flight Centre',
         title: 'Sun City Soho Mid week Rate',
@@ -76,7 +76,7 @@ export const DATA = [
         specialPrice: 4055
       },
       {
-        link: '../images/flight-centre/The Palace of the Lost City.jpg',
+        link: 'images/flight-centre/lost-city-pool.jpg',
         id: 5,
         name: 'Flight Centre',
         title: 'The Palace of the Lost City',
@@ -87,7 +87,7 @@ export const DATA = [
         specialPrice: 6475
       },
       {
-        link: '../images/flight-centre/The Palace of the Lost City golf course.jpg',
+        link: 'images/flight-centre/lost-city-golf.jpg',
         id: 6,
         name: 'Flight Centre',
         title: 'The Palace of the Lost City',
@@ -98,7 +98,7 @@ export const DATA = [
         specialPrice: 6475
       },
       {
-        link: '../images/flight-centre/oyster-box-umhlanga-rocks.jpg',
+        link: 'images/flight-centre/oyster-box-umhlanga-rocks.jpg',
         id: 7,
         name: 'Flight Centre',
         title: 'The Oyster Box Hotel - Umhlanga',
@@ -109,7 +109,7 @@ export const DATA = [
         specialPrice: 8245
       },
       {
-        link: '../images/flight-centre/12 Apostles Hotel Dining 2.jpg',
+        link: 'images/flight-centre/12-apostles.jpg',
         id: 8,
         name: 'Flight Centre',
         title: 'The 12 Apostles Hotel & Spa',
@@ -120,7 +120,7 @@ export const DATA = [
         specialPrice: 8245
       },
       {
-        link: '../images/flight-centre/Mabula Game viewing 2.jpg',
+        link: 'images/flight-centre/mabula.jpg',
         id: 9,
         name: 'Flight Centre',
         title: 'Mabula Game Lodge',
@@ -131,7 +131,7 @@ export const DATA = [
         specialPrice: 4855
       },
       {
-        link: '../images/flight-centre/Beverley Hills Pool.jpg',
+        link: 'images/flight-centre/Beverley-Hills-Pool.jpg',
         id: 10,
         name: 'Flight Centre',
         title: 'The Beverly Hills Hotel - Umhlanga',
@@ -149,63 +149,63 @@ export const DATA = [
     background: 'images/accommodation-main.jpg',
     clients: [
       {
-        link: 'offer1.PNG',
+        link: 'logos/offer1.PNG',
         name: 'A Zambezi River Lodge',
         text:
           'A’Zambezi River Lodge is a 6-time (consecutive) TripAdvisor service excellence award, winner',
         textAvatar: '../../assets/logos/RTG_mini.PNG',
       },
       {
-        link: 'offer2.PNG',
+        link: 'logos/offer2.PNG',
         name: 'Victoria Falls Rainbow Hotel',
         text:
           'The hotel’s central location means that you’re within walking distance of the mighty Victoria Falls and other awesome activities around the resort town',
         textAvatar: '../../assets/logos/RTG_mini.PNG',
       },
       {
-        link: 'offer3-new.PNG',
+        link: 'logos/offer3-new.PNG',
         name: 'Kadoma Hotel and Conference Centre',
         text:
           'Kadoma Hotel & Conference Centre is the ideal getaway destination for both conferencing and leisure',
         textAvatar: '../../assets/logos/RTG_mini.PNG',
       },
       {
-        link: 'offer4.PNG',
+        link: 'logos/offer4.PNG',
         name: 'Rainbow Towers Hotel & Conference Centre',
         text:
           'The luxurious iconic 5-star hotel offers a unique and refreshing experience for both business and leisure customers in the sunshine city',
         textAvatar: '../../assets/logos/RTG_mini.PNG',
       },
       {
-        link: 'offer5.PNG',
+        link: 'logos/offer5.PNG',
         name: 'New Ambassador Hotel',
         text:
           'The New Ambassador Hotel has a rich history as the epicenter of black consciousness. Located in Harare’s Central Business District, the boutique hotel is within reach of restaurants, cinemas, the National Art Gallery and other service centers',
         textAvatar: '../../assets/logos/RTG_mini.PNG',
       },
       {
-        link: 'offer6-new.PNG',
+        link: 'logos/offer6-new.PNG',
         name: 'Monomotapa Hotel Harare',
         text:
           'The facilities at Monomotapa are specially designed for the convenience of the guest. The hotel provides a one-stop service where accommodation and other services are all available',
         textAvatar: '../../assets/logos/RTG_mini.PNG',
       },
       {
-        link: 'offer7.PNG',
+        link: 'logos/offer7.PNG',
         name: 'Bulawayo Rainbow Hotel',
         text:
           'The hotel is a 15 minute drive from Joshua Mqabuko Nkomo Airport and is within reach of express coaches connecting Harare and Victoria Falls',
         textAvatar: '../../assets/logos/RTG_mini.PNG',
       },
       {
-        link: 'offer8-new.PNG',
+        link: 'logos/offer8-new.PNG',
         name: 'Cresta Lodge Harare',
         text:
           'Set in beautiful and tranquil surroundings, located on the outskirts of the dynamic city centre, you’ll find the beautiful Cresta Lodge, one of the best hotels in Harare',
         textAvatar: '../../assets/logos/RTG_mini.PNG',
       },
       {
-        link: 'offer9-new.PNG',
+        link: 'logos/offer9-new.PNG',
         name: 'Cresta Spray View Victoria Falls',
         text:
           'Topping the list of affordable Victoria Falls hotels, Cresta Sprayview offers stylish, well-appointed accommodation and a host of convenient facilities for business and leisure travelers alike',
@@ -219,24 +219,24 @@ export const DATA = [
     background: 'images/dining-main.jpg',
     clients: [
       {
-        link: 'offer10.jpg',
+        link: 'logos/offer10.jpg',
         name: 'Le Rendezvous',
         text:
           'Situated on the Mezzanine Floor, the coffee shop offers a wide range of coffee and teas along with a selection of light meals, cakes and pastries. The coffee shop is ideal for relaxing with family and friends or for a business meeting',
       },
       {
-        link: 'restaurant.jpeg',
+        link: 'logos/restaurant.jpeg',
         name: 'Mojos',
         text: "Come to Mojo's and eat as much meat as you want",
       },
       {
-        link: 'Kombahari.PNG',
+        link: 'logos/Kombahari.PNG',
         name: 'Kombahari Restaurant',
         text:
           'The best Afro-Asian fusion dining experience and only live cooking restaurant in the country.',
       },
       {
-        link: 'restaurant2.jpeg',
+        link: 'logos/restaurant2.jpeg',
         name: 'Alo Alo',
         text:
           'A Home away from Home Experience surrounded by a beautiful tranquil garden',
